75edf72cbfde50cdd3da514f231f99592734f8fb
ShahrozKhan-1 <132278376+ShahrozKhan-1@users.noreply.github.com>
2025-02-01 20:39:03 +0500

    Delete E-commerce directory

--- E-commerce/Backend/ecomapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import *
-
-
-urlpatterns = [
-    path('first/', first),
-    path('products/', getProducts, name="Get_products"),
-    path('products/<str:pk>/', singleProduct, name="Get_Single_products"),
-]
